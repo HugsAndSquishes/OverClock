9f1d6d309d4efa2b4e2fd35650ce7597953be851
Jacky <cunkuaiz0850@gmail.com>
2025-02-20 19:22:35 -0500

    Added test case for Punch model

--- punches/tests.py
+++ punches/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
 
-# Create your tests here.
+
+from django.test import TestCase
+from .models import Punch
+
+class PunchModelTest(TestCase):
+    def test_punch_creation(self):
+        punch = Punch.objects.create(user="John Doe", punch_type="IN")
+        self.assertEqual(punch.punch_type, "IN")
