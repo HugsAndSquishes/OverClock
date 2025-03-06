b19f9cb787d3a18278a313feed4bf6ef9003024c
Jacky <cunkuaiz0850@gmail.com>
2025-03-06 16:14:31 -0500

    Implemented CRUD operations for Attendance in views.py

--- punches/views.py
+++ punches/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Attendance
+from .forms import AttendanceForm
 
-# Create your views here.
+def attendance_list(request):
+    attendances = Attendance.objects.all()
+    return render(request, 'punches/attendance_list.html', {'attendances': attendances})
+
+def attendance_detail(request, pk):
+    attendance = get_object_or_404(Attendance, pk=pk)
+    return render(request, 'punches/attendance_detail.html', {'attendance': attendance})
+
+def attendance_create(request):
+    if request.method == 'POST':
+        form = AttendanceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('attendance_list')
+    else:
+        form = AttendanceForm()
+    return render(request, 'punches/attendance_form.html', {'form': form})
+
+def attendance_update(request, pk):
+    attendance = get_object_or_404(Attendance, pk=pk)
+    if request.method == 'POST':
+        form = AttendanceForm(request.POST, instance=attendance)
+        if form.is_valid():
+            form.save()
+            return redirect('attendance_list')
+    else:
+        form = AttendanceForm(instance=attendance)
+    return render(request, 'punches/attendance_form.html', {'form': form})
+
+def attendance_delete(request, pk):
+    attendance = get_object_or_404(Attendance, pk=pk)
+    if request.method == 'POST':
+        attendance.delete()
+        return redirect('attendance_list')
+    return render(request, 'punches/attendance_confirm_delete.html', {'attendance': attendance})
