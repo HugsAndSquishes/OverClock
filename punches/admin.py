46221f0a6348b6c923aba59b6bf9ac704ed616bf
GoddessOfNutella <51200675+GoddessOfNutella@users.noreply.github.com>
2025-03-31 20:48:05 -0400

    Attendances are now listed by employee name and the date. Clicking employee name now shows a list of Attendances

--- punches/admin.py
+++ punches/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from . import models
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 # Register your models here.
 class AttendanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('get_employee_name', 'date')
+
+    @admin.display(description='Author Name', ordering='author__name')
+    def get_employee_name(self, obj):
+        return obj.user.last_name + ", " + obj.user.first_name
+
+
+class AttendanceInline(admin.StackedInline):
+    model = models.Attendance
+    can_delete = False
+    verbose_name_plural = "Attendances"
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('last_name', 'first_name',)
+    inlines = [AttendanceInline]
+
 
 admin.site.register(models.Attendance, AttendanceAdmin) 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
